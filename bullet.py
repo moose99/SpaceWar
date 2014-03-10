360129041d54a5ac43375239a1ff36ad5262496c
mus <mus@thamers.com>
2014-03-09 21:45:07 -0400

    better bullet firing

--- bullet.py
+++ bullet.py
@@ -5,7 +5,7 @@ __author__ = 'Mus'
 import math
 
 # bullet constant values
-BULLET_SPEED = 2
+BULLET_SPEED = 8
 
 # initialize variables to hold the bullet rectangle and image
 bulletRect = []
@@ -29,23 +29,39 @@ def FireBullet(game, x, y, rotation):
     numBullets += 1
     BULLET_WIDTH = 8
     BULLET_HEIGHT = 8
+    dirX = math.cos(math.radians(rotation))
+    dirY = -math.sin(math.radians(rotation))
+    x += dirX
+    y += dirY
     bulletPosX.insert(0, x)
     bulletPosY.insert(0, y)
     rect = game.Rect(x-(BULLET_WIDTH/2), y-(BULLET_HEIGHT/2), BULLET_WIDTH, BULLET_HEIGHT)
     bulletRect.insert(0, rect)
-
-    bulletDirX.insert(0, math.cos(math.radians(rotation)))
-    bulletDirY.insert(0, -math.sin(math.radians(rotation)))    # flip Y
+    bulletDirX.insert(0, dirX)
+    bulletDirY.insert(0, dirY)    # flip Y
     bulletSound.play()
 
-def MoveBullets():
+def RemoveBullet(i):
+    global numBullets
+    del bulletPosX[i]
+    del bulletPosY[i]
+    del bulletRect[i]
+    del bulletDirX[i]
+    del bulletDirY[i]
+    numBullets -= 1
+
+def MoveBullets(surface):
     global bulletRect, bulletPosX, bulletPosY
-    for i in range(numBullets):
+    i = numBullets-1
+    while (i>=0):
         bulletPosX[i] += bulletDirX[i] * BULLET_SPEED
         bulletPosY[i] += bulletDirY[i] * BULLET_SPEED
         bulletRect[i].center = (bulletPosX[i], bulletPosY[i])
+        if (bulletPosX[i] > surface.get_width() or bulletPosY[i] > surface.get_height() or bulletPosX[i] < 0 or bulletPosY[i] < 0):
+            RemoveBullet(i)
+        i -= 1
 
 def DrawBullet(surface):
-    MoveBullets()
+    MoveBullets(surface)
     for rect in bulletRect:
         surface.blit(bulletImage, rect)
